87f7fdeccc9920e3ab06501a562bd1395804e03a
pandavandama <strangemikados@gmail.com>
2020-03-04 00:00:02 +0200

    new project

---
+++ Blocks/Header.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="head_wrapper">
+        <div class="head_elem"></div>
+        <div class="head_elem"></div>
+        <div class="head_elem"></div>
+        <div class="head_elem"></div>
+    </div>
+
+</header>
