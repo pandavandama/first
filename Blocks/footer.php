0689ecb4a958177ee2ade2d6b44be944172bcbb3
constantinc <constantinc@usalinksystem.com>
2020-03-10 16:40:09 +0200

    begin

---
+++ Blocks/footer.php
@@ -0,0 +1,13 @@
+<footer>
+        <div class="foot_wrapper">
+            <div class="foot_elem">
+
+            </div>
+            <div class="foot_elem">
+                
+            </div>
+            <div class="foot_elem">
+                
+            </div>
+        </div>
+    </footer>
--- index.php
+++ index.php
@@ -3,29 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Project</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+  
+
+<section class="ad">
+
+</section>
+<section class="ad_right">
+
+</section>
 <?php require "Blocks/Header.php" ?>
-    <main>
     
+    <main>
+    <div class="in_1">
+    
+    <form method="get">
+    <input type="text" name="name" id="name_1">
+    <input type="submit" value="Отправить">
+    <input type="reset" value="Очистить">
+    </div>
+    </form>
+    <?php echo htmlspecialchars($_GET['name'])?>
     </main>
     
-    <footer>
-        <div class="foot_wrapper">
-            <div class="foot_elem">
-
-            </div>
-            <div class="foot_elem">
-                
-            </div>
-            <div class="foot_elem">
-                
-            </div>
-        </div>
-    </footer>
+    
+<?php require "Blocks/footer.php" ?>
 
 
 </body>
